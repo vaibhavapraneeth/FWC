06d33f81a020f60f5651283cd37fd62285656548
vaibhavapraneeth <58224823+vaibhavapraneeth@users.noreply.github.com>
2023-02-20 12:06:00 +0530

    Update main.c

--- fwc/vectormaths/12.10.3.2/cprog/main.c
+++ fwc/vectormaths/12.10.3.2/cprog/main.c
@@ -14,11 +14,11 @@ int main()
 	b=loadtxt("b.dat",3,1);		//loading the point B from the text file
 	d=linalg_norm(a,m);		//Finding the norm of the matrix A
 	e=linalg_norm(b,m);		//Finding the norm of the matrix B	
-	c=transpose(a,m,n);
-	f=d*e;
-	g= **matmul(c,b,1,3,1);
-	h=(1/f)*g;
-	i=acos(h)*180/PI;
+	c=transpose(a,m,n);		//Finding the transpose of the Matrix A
+	f=d*e;				//multiplying normA and normB
+	g= **matmul(c,b,1,3,1);		// Matrix multipilcation of A transpose and B
+	h=(1/f)*g;			// finding the cos(g/f)
+	i=acos(h)*180/PI;		// finding the angle between A and B
 	printf("\n The angle between a and b is %.2lf degrees",i);	//printing angle of A
 
 }
