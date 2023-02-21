1a736772f34b6f3fc48722e8ab03693851db7580
vaibhavapraneeth <58224823+vaibhavapraneeth@users.noreply.github.com>
2023-02-21 12:35:38 +0530

    Update main.c

--- fwc/vectormaths/12.10.4.4/cprog/main.c
+++ fwc/vectormaths/12.10.4.4/cprog/main.c
@@ -11,11 +11,15 @@ int main()
 	int m=2,n=1;
 	a=loadtxt("a.dat",2,1);		//loading the point A from the text file
 	b=loadtxt("b.dat",2,1);		//loading the point B from the text file
-	c=linalg_add(a,b,m,n);
-	d=linalg_sub(a,b,m,n);
-	e= cross_prod(a,b);
-	g=2*e;
-	f= cross_prod(d,c);
-	printf("\n the cross prod of a,b is %lf",e);
-	printf("\n the cross prod of c,d is %lf",f);
+	c=linalg_add(a,b,m,n);		// addind A and B (A+B)
+	d=linalg_sub(a,b,m,n);		// subtracting B from A (A-B)
+	e= cross_prod(a,b)		// E= (A X B)
+	g=2*e;				// G= 2(A X B)
+	f= cross_prod(d,c);		// (A-B) X (A+B)
+	printf("\n the cross prod of a,b is %lf",e); // printing A X B
+	printf("\n the cross prod of c,d is %lf",f); // printing (A-B) X (A+B)
+	if(g==f)
+	{
+	 printf("\n so 2(A X B) = (A-B) X (A+B)");
+	}
 }
